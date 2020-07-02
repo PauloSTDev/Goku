9808fc0333e7ceddeff3c0d0e7249287fbc17c26
PauloSantos <afonso.mea@gmail.com>
2020-07-01 23:33:06 -0300

    Criação de Cenário e Objeto de movimentação

--- mygame.py
+++ mygame.py
@@ -1,14 +1,33 @@
 import pygame
 import time
 import random
+
 pygame.init()
 
-tela_Altura=600
+tela_Altura= 600
 tela_Largura=800
-pygame.display.set_caption("Sonic Run")
+pygame.display.set_caption("Goku In The Space")
 gameDisplay=pygame.display.set_mode((tela_Largura, tela_Altura))
-icone=pygame.image.load("assets/icone.png")
-pygame.display.set_icon(icone)
-white=(255,255,255)
+fundo =pygame.image.load("assets/espaco.jpg")
+
 clock=pygame.time.Clock()
-gameDisplay.fill(white)
+
+nave=pygame.image.load("assets/GokuNave.gif")
+padrao_X=340
+padrao_Y=400
+
+### funções ####
+def mostrarNave(x,y):
+    gameDisplay.blit( nave, (x,y))
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    gameDisplay.blit(fundo, (0,0))
+    mostrarNave(padrao_X,padrao_Y)
+    pygame.display.update()
+    clock.tick(60)
+    
